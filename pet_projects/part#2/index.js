db0f19b42c60516ad586a7efa6b8bfd5c77dbb4c
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2022-06-01 19:44:32 -0600

    challenge 2.1

--- pet_projects/part#2/index.js
+++ pet_projects/part#2/index.js
@@ -2,8 +2,8 @@
 § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 */
-
-const dolphins_1 = [44, 23 ,71];
+//challenge_1
+/*const dolphins_1 = [44, 23 ,71];
 const dolphins_2 = [85, 54 , 41];
 const koalas_1 = [65, 54 , 49];
 const koalas_2 = [23, 34 , 27];
@@ -17,5 +17,25 @@ console.log(data1);
 checkWinner(data1);
 console.log(data2);
 checkWinner(data2);
+*/
+
+//challenge_2
+
+const billArr = [125, 555,  44];
+
+const calcTip = function(billVal){
+    const getPercentage = (num, perc) => Math.round((num/100)*perc);
+    return billVal > 50 && billVal < 300? getPercentage(billVal, 15): getPercentage(billVal, 20)
+};
+
+const tips = billArr.reduce((acc, el) => acc.concat(calcTip(el)) ,[]);
+
+const total = billArr.map((el, idx) => el + tips[idx]);
+
+console.log(billArr);
+console.log(tips);
+console.log(total);
+
+
 
 
